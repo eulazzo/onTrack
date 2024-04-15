b1e2e5e44bc6909ef8051ecee3c9b8437b60bc20
eulazzo <lazarovanderson@gmail.com>
2024-04-14 22:49:50 -0300

    chore: added primevue and tailwind libs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import PrimeVue from 'primevue/config';
+import Lara from './presets/lara'; 
 import router from './router'
 
 const app = createApp(App)
+app.use(PrimeVue, {
+    unstyled: true,
+    pt: Lara  
+});
 app.use(router)
 app.mount('#app')
 
